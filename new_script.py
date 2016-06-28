26ca0afc653bec798d8a9619f8f3a153bef32bde
NaStyA128 <n.a.s.t.y.a28v@gmail.com>
2016-06-28 16:08:14 +0300

    Volume and weight

--- new_script.py
+++ new_script.py
@@ -1,3 +1,6 @@
+import types
+
+
 class Properties:
 
     material = None
@@ -11,13 +14,16 @@ class Properties:
         self.coordinate = kwargs.pop('coordinate')
         x = self.coordinate['bottom_right'][0] - self.coordinate['top_left'][0]
         y = self.coordinate['bottom_right'][1] - self.coordinate['top_left'][1]
-        self.length = x
-        self.width = y
+        self.length = float(x)
+        self.width = float(y)
 
 
 class Furniture(Properties):
 
+    weight = None
+
     def __init__(self, **kwargs):
+        self.weight = float(kwargs.pop('weight'))
         super().__init__(**kwargs)
 
 
@@ -27,8 +33,8 @@ class Bed(Furniture):
     mattress = None    # матрац
 
     def __init__(self, **kwargs):
-        self.linen = kwargs.pop('linen')
-        self.mattress = kwargs.pop('mattress')
+        self.linen = bool(kwargs.pop('linen'))
+        self.mattress = bool(kwargs.pop('mattress'))
         super().__init__(**kwargs)
 
 
@@ -38,7 +44,7 @@ class Table(Furniture):
     coating = None    # покрытие
 
     def __init__(self, *kwargs):
-        self.number_of_legs = kwargs.pop('number_of_legs')
+        self.number_of_legs = int(kwargs.pop('number_of_legs'))
         self.coating = kwargs.pop('coating')
         super().__init__(**kwargs)
 
@@ -49,7 +55,7 @@ class Cupboard(Furniture):
     coating = None    # покрытие
 
     def __init__(self, **kwargs):
-        self.number_of_shelves = kwargs.pop('number_of_shelves')
+        self.number_of_shelves = int(kwargs.pop('number_of_shelves'))
         self.coating = kwargs.pop('coating')
         super().__init__(**kwargs)
 
@@ -61,7 +67,7 @@ class Chair(Furniture):
 
     def __init__(self, **kwargs):
         self.upholstery = kwargs.pop('upholstery')
-        self.number_of_legs = kwargs.pop('number_of_legs')
+        self.number_of_legs = int(kwargs.pop('number_of_legs'))
         super().__init__(**kwargs)
 
 
@@ -72,7 +78,38 @@ class Nightstand(Furniture):
 
     def __init__(self, **kwargs):
         self.coating = kwargs.pop('coating')
-        self.number_of_boxes = kwargs.pop('number_of_boxes')
+        self.number_of_boxes = int(kwargs.pop('number_of_boxes'))
+        super().__init__(**kwargs)
+
+
+class Bath(Furniture):
+
+    volume = None
+    shower = None    # душ
+
+    def __init__(self, **kwargs):
+        self.volume = float(kwargs.pop('volume'))
+        self.shower = bool(kwargs.pop('shower'))
+        super().__init__(**kwargs)
+
+
+class Sink(Furniture):
+
+    mixer = None    # смеситель
+    mirror = None    # зеркало
+
+    def __init__(self, **kwargs):
+        self.mixer = bool(kwargs.pop('mixer'))
+        self.mirror = bool(kwargs.pop('mirror'))
+        super().__init__(**kwargs)
+
+
+class Toilet(Furniture):
+
+    cover = None    # крышка
+
+    def __init__(self, **kwargs):
+        self.cover = bool(kwargs.pop('cover'))
         super().__init__(**kwargs)
 
 
@@ -80,11 +117,23 @@ class Appliances(Properties):
 
     category = None    # категория
     provider = None    # производитель
+    power = None    # мощность (W or Вт)
+    mean_time = None    # среднее время работы (hours/day)
+    weight = None
+    energy = None
 
     def __init__(self, **kwargs):
+        self.power = float(kwargs.pop('power'))
+        self.weight = float(kwargs.pop('weight'))
         self.category = kwargs.pop('category')
         self.provider = kwargs.pop('provider')
         super().__init__(**kwargs)
+
+    def energy_consumption(self):
+        a = self.power * self.mean_time
+        a /= 1000
+        a *= 30    # kWh / month
+        self.energy = a
 
 
 class Refrigerator(Appliances):
@@ -96,19 +145,18 @@ class Refrigerator(Appliances):
 
     def __init__(self, **kwargs):
         self.type = kwargs.pop('type')
-        self.max_temp = kwargs.pop('max_temp')
-        self.min_temp = kwargs.pop('min_temp')
-        self.capacity = kwargs.pop('capacity')
+        self.max_temp = float(kwargs.pop('max_temp'))
+        self.min_temp = float(kwargs.pop('min_temp'))
+        self.capacity = float(kwargs.pop('capacity'))
         super().__init__(**kwargs)
 
 
 class Microwave(Appliances):
 
-    power = None    # мощность
+    clock = None
 
     def __init__(self, **kwargs):
-        self.power = kwargs.pop('power')
-        self.clock = kwargs.pop('clock')
+        self.clock = bool(kwargs.pop('clock'))
         super().__init__(**kwargs)
 
 
@@ -120,48 +168,19 @@ class Stove(Appliances):
 
     def __init__(self, **kwargs):
         self.type = kwargs.pop('type')
-        self.number_of_burners = kwargs.pop('number_of_burners')
-        self.oven = kwargs.pop('oven')
-        super().__init__(**kwargs)
-
-
-class Bath(Appliances):
-
-    volume = None
-    shower = None    # душ
-
-    def __init__(self, **kwargs):
-        self.volume = kwargs.pop('volume')
-        self.shower = kwargs.pop('shower')
-        super().__init__(**kwargs)
-
-
-class Sink(Appliances):
-
-    mixer = None    # смеситель
-    mirror = None    # зеркало
-
-    def __init__(self, **kwargs):
-        self.mirror = kwargs.pop('mirror')
-        self.mixer = kwargs.pop('mixer')
-        super().__init__(**kwargs)
-
-
-class Toilet(Appliances):
-
-    cover = None    # крышка
-
-    def __init__(self, **kwargs):
-        self.cover = kwargs.pop('cover')
+        self.number_of_burners = int(kwargs.pop('number_of_burners'))
+        self.oven = bool(kwargs.pop('oven'))
         super().__init__(**kwargs)
 
 
 class Windows(Properties):
 
     type = None    # окно / дверь
+    weight = None
 
     def __init__(self, **kwargs):
         self.type = kwargs.pop('type')
+        self.weight = float(kwargs.pop('weight'))
         super().__init__(**kwargs)
 
 
@@ -174,12 +193,22 @@ class Room(Properties):
     def __init__(self, **kwargs):
         self.furniture = kwargs.pop('furniture')
         self.windows = kwargs.pop('windows')
-        self.height = kwargs.pop('height')
+        self.height = float(kwargs.pop('height'))
         super().__init__(**kwargs)
 
     @property
     def volume(self):
         return self.length * self.width * self.height
+
+    @property
+    def weight(self):
+        s = sum([f.weight for f in self.furniture])
+        s += sum(w.weight for w in self.windows)
+        return s
+
+    @property
+    def energy(self):
+        pass
 
 
 class Kitchen(Room):
@@ -209,6 +238,10 @@ class House(Properties):
     def total_volume(self):
         return sum([room.volume for room in self.rooms])
 
+    @property
+    def total_weight(self):
+        return sum([room.weight for room in self.rooms])
+
 
 house = House(rooms=[
     Kitchen(furniture=[
@@ -220,13 +253,17 @@ house = House(rooms=[
                      coordinate={'top_left': (0, 0),
                                  'bottom_right': (100, 80)},
                      category='Kitchen',
-                     provider='Beko')
+                     provider='Beko',
+                     weight=10,
+                     power=250,
+                     mean_time=5)
         ],
         windows=[
             Windows(type='window',
                     material='glass',
                     coordinate={'top_left': (0, 0),
-                                'bottom_right': (100, 80)})
+                                'bottom_right': (100, 80)},
+                    weight=10)
         ],
         height=300,
         material='brick',
@@ -235,4 +272,6 @@ house = House(rooms=[
     )
 ])
 
-print(house.total_volume)
+# print(type(house) == types)
+print('Volume of house:', house.total_volume)
+print('Weight of house:', house.total_weight)
